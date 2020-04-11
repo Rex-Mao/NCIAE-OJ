f40397f4e44378d99a5d0a86fc6c76c798ba0079
RexMao <a568198246z@vip.qq.com>
2020-04-11 23:43:41 +0800

    Fix
    update the userinfo mapper

---
+++ user-center/src/main/java/cn/edu/nciae/usercenter/common/mapper/UserInfoMapper.java
@@ -0,0 +1,18 @@
+package cn.edu.nciae.usercenter.common.mapper;
+
+import cn.edu.nciae.usercenter.common.entity.UserInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author RexALun
+ * @since 2020-04-04
+ */
+@Component
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+
+}
